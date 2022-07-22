e18be8ea3f641f863ad18750b2210669124f4df8
amanda-santos <amandasf0200@gmail.com>
2022-07-22 20:40:05 -0300

    feat: ✨ improve form and validation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import { ReactElement } from 'react'
 import { Play } from 'phosphor-react'
+import * as zod from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import {
   CountdownContainer,
@@ -11,10 +14,39 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod
+    .string()
+    .min(2, 'Task name is too short')
+    .max(50, 'Task name is too long'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'The cycle needs to be at least 5 minutes.')
+    .max(60, 'The cycle must be a maximum of 60 minutes.'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export const Home = (): ReactElement => {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 5,
+    },
+  })
+
+  const handleCreateNewCycle = (data: any) => {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task || task.length < 3
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">I&apos;ll work on</label>
           <TaskInput
@@ -22,6 +54,7 @@ export const Home = (): ReactElement => {
             id="task"
             list="task-suggestions"
             placeholder="Give a name to your task"
+            {...register('task')}
           />
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
@@ -38,6 +71,7 @@ export const Home = (): ReactElement => {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutes.</span>
@@ -51,7 +85,7 @@ export const Home = (): ReactElement => {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
           <Play size={24} />
           Start
         </StartCountdownButton>
